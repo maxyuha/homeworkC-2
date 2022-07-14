99c3c753449751a970ba5fab1e8be2b6a4004e78
Maksim <maxyuha1998@gmail.com>
2022-07-14 15:39:44 +0300

    make task1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+        void Domashka()
+        {
+            string number=Console.ReadLine();
+            if (number.Length>2)Console.WriteLine(number[2]);
+            else Console.WriteLine("в числе меньше 3 символов");
+            
+            
+        }
+        Domashka();
+     }
+    
+ }
+} 
